103aaca87389141a36d14bdf5abf9a4a26317040
ronvillela <ronvillela@me.com>
2021-10-03 23:28:33 -0400

    add picture function

---
+++ android-rest-client-template-master/app/src/main/java/com/codepath/apps/restclienttemplate/TweetDialogFragment.java
@@ -0,0 +1,54 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class TweetDialogFragment extends DialogFragment {
+
+    private EditText mEditText;
+
+
+    public TweetDialogFragment() {}
+
+    public static TweetDialogFragment newInstance(String title) {
+        TweetDialogFragment frag = new TweetDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_compose, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Get field from view
+        mEditText = (EditText) view.findViewById(R.id.etCompose);
+        // Fetch arguments from bundle and set title
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+        // Show soft keyboard automatically and request focus to field
+        mEditText.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        // 2. Setup a callback when the "Done" button is pressed on keyboard
+
+    }
+
+
+}
